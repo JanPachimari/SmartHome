c5974cc5a021710e564f96db786094920e95d752
Jan Tekautschitz <jan.tekautschitz@rub.de>
2020-09-15 16:57:44 +0200

    PUT-Request für Aufgabe 5 funktioniert

--- app/src/main/java/rub/jantekautschitz/androidpraktikum/Activity4.kt
+++ app/src/main/java/rub/jantekautschitz/androidpraktikum/Activity4.kt
@@ -1,21 +1,27 @@
 package rub.jantekautschitz.androidpraktikum
 
-import android.app.Activity
 import android.content.Context
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
 
 
 class Activity4 : AppCompatActivity(), SensorEventListener {
 
     private lateinit var sensorManager: SensorManager
     private var lichtstufe: Sensor? = null              // speichert aktuellen Lichtwert
+
+    var grenzwert : Double = 100.00
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -38,6 +44,51 @@ class Activity4 : AppCompatActivity(), SensorEventListener {
         val neueLichtstufe = event.values[0]
         var textHelligkeit : TextView = findViewById<TextView>(R.id.textHelligkeit)
         textHelligkeit.text = "${neueLichtstufe}"           // aktualisiere momentanen Wert in TextView
+
+        var unterGrenzwert : Boolean = true
+
+        if(neueLichtstufe > grenzwert) {
+            unterGrenzwert = false
+            Log.d("D", "Licht ist größer als Grenzwert")
+        }
+
+        if(neueLichtstufe < grenzwert) {     // Grenzwert unterschritten
+
+            Log.d("D", "Licht ist kleiner als Grenzwert")
+
+            val client = OkHttpClient()
+            val postBody = "ON"
+
+            val request = Request.Builder()
+                .url("https://smarthome-imtm.iaw.ruhr-uni-bochum.de/rest/items/GF_LivingRoom_Light/state")
+                .put(postBody.toRequestBody(MEDIA_TYPE_PLAIN))
+                .build()
+
+            client.newCall(request).enqueue(object : Callback {
+                override fun onFailure(call: Call, e: IOException) {
+                    e.printStackTrace()
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    response.use {
+                        if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                        for ((name, value) in response.headers) {
+                            println("$name: $value")
+                        }
+
+                        println(response.body!!.string())
+                    }
+                }
+            })
+
+            Toast.makeText(
+                this@Activity4,
+                "Das Licht wurde eingeschaltet!",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
     }
 
     override fun onPause() {        // falls App pausiert, setze diese Activity als zuletzt benutzt
@@ -50,4 +101,7 @@ class Activity4 : AppCompatActivity(), SensorEventListener {
         sensorManager.unregisterListener(this)      // unterbreche Datenstrom bei Pausieren der App
     }
 
+    companion object {
+        val MEDIA_TYPE_PLAIN = "text/plain; charset=utf-8".toMediaType()
+    }
 }
