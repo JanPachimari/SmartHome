7b0f5993758897d16f6ebc800fad60c20ed2c1c6
Jan Tekautschitz <jan.tekautschitz@rub.de>
2020-09-16 11:30:45 +0200

    Aufgabe 6 Kamera und Bilder speichern funktioniert

--- app/src/main/java/rub/jantekautschitz/androidpraktikum/Activity5.kt
+++ app/src/main/java/rub/jantekautschitz/androidpraktikum/Activity5.kt
@@ -2,9 +2,17 @@ package rub.jantekautschitz.androidpraktikum
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.FileProvider
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import java.io.File
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class Activity5 : AppCompatActivity() {
@@ -14,11 +22,60 @@ class Activity5 : AppCompatActivity() {
 
         var kameraButton : FloatingActionButton = findViewById<FloatingActionButton>(R.id.kameraButton)
         kameraButton.setOnClickListener {
-            val intent = Intent("android.media.action.IMAGE_CAPTURE")
-            startActivity(intent)
+
+            dispatchTakePictureIntent()
+
         }
 
     }
+
+    lateinit var currentPhotoPath : String
+
+    @Throws(IOException::class)
+    private fun createImageFile(): File {
+        // Create an image file name
+        val timeStamp: String = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        val storageDir: File = this!!.getExternalFilesDir(Environment.DIRECTORY_PICTURES)!!
+        return File.createTempFile(
+            "JPEG_${timeStamp}_", /* prefix */
+            ".jpg", /* suffix */
+            storageDir /* directory */
+        ).apply {
+            // Save a file: path for use with ACTION_VIEW intents
+            currentPhotoPath = absolutePath
+        }
+    }
+
+    val REQUEST_TAKE_PHOTO = 1
+
+    private fun dispatchTakePictureIntent() {
+        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
+            // Ensure that there's a camera activity to handle the intent
+            takePictureIntent.resolveActivity(packageManager)?.also {
+                // Create the File where the photo should go
+                val photoFile: File? = try {
+                    createImageFile()
+                } catch (ex: IOException) {
+                    // Error occurred while creating the File
+                    null
+                }
+                // Continue only if the File was successfully created
+                photoFile?.also {
+                    val photoURI: Uri = FileProvider.getUriForFile(
+                        this,
+                        "rub.jantekautschitz.androidpraktikum.fileprovider",
+                        it
+                    )
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
+
+
+
+                }
+            }
+        }
+    }
+
 
     override fun onPause() {        // falls App pausiert, setze diese Activity als zuletzt benutzt
         super.onPause()
