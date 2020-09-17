8f9b648a67cb88fe62dd479beb636c2ab4b5ff08
Jan Tekautschitz <jan.tekautschitz@rub.de>
2020-09-17 10:27:58 +0200

    Aufgabe 8 NFC-Handler erstellt

--- app/src/main/java/rub/jantekautschitz/androidpraktikum/Activity7.kt
+++ app/src/main/java/rub/jantekautschitz/androidpraktikum/Activity7.kt
@@ -1,16 +1,48 @@
 package rub.jantekautschitz.androidpraktikum
 
 import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class Activity7 : AppCompatActivity() {
+
+    var mNfcAdapter: NfcAdapter? = null
+    var nfcText: TextView = findViewById<TextView>(R.id.nfcText)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_7)
+
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this);       // NFC-Adapter
+
+        if (mNfcAdapter == null) {
+            Toast.makeText(this, "Dieses Gerät unterstützt NFC nicht.", Toast.LENGTH_LONG).show();
+            finish();                           // beende Activity, falls Gerät NFC nicht unterstützt
+            return;
+        }
+
+        if (!mNfcAdapter!!.isEnabled()) {       // NFC ist auf dem Gerät nicht eingeschaltet
+            nfcText.text="NFC ist ausgeschaltet."
+        }
+
+        handleIntent(getIntent());
     }
 
-    // todo
+    private fun handleIntent(intent: Intent?) {
+        if (intent != null) {
+            if (NfcAdapter.ACTION_TAG_DISCOVERED.equals(intent.getAction())) {
+                // TODO: process intent
+            }
+        }
+        else {
+            Log.d("NFC", "Fehler: Intent = null")
+        }
+    }
 
     override fun onPause() {        // falls App pausiert, setze diese Activity als zuletzt benutzt
         super.onPause()
@@ -20,4 +52,6 @@ class Activity7 : AppCompatActivity() {
         editor.putString("lastActivity", javaClass.name)
         editor.commit()
     }
+
+
 }
