52dcad3df44f011a7b28f734f56f4a3b8fc53732
Jan Tekautschitz <jan.tekautschitz@rub.de>
2020-09-17 12:42:21 +0200

    Support für Landscape-Modus

--- app/src/main/java/rub/jantekautschitz/androidpraktikum/HomeActivity.kt
+++ app/src/main/java/rub/jantekautschitz/androidpraktikum/HomeActivity.kt
@@ -1,5 +1,6 @@
 package rub.jantekautschitz.androidpraktikum
 
+import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -46,4 +47,12 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+    override fun onPause() {        // falls App pausiert, setze diese Activity als zuletzt benutzt
+        super.onPause()
+        val prefs =
+            getSharedPreferences("LetzteActivity", Context.MODE_PRIVATE)
+        val editor = prefs.edit()
+        editor.putString("lastActivity", javaClass.name)
+        editor.commit()
+    }
 }
